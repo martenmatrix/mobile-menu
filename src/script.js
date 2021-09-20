99710a7a86fee66103d7e12782b12c5551001b4d
Marten <derapfelesser@aol.com>
2021-09-20 18:51:17 +0200

    Selected section now gets selected class and redirect works

--- src/script.js
+++ src/script.js
@@ -3,11 +3,19 @@ export default function initMenu() {
     const menuButtons = document.querySelectorAll('.menu-button.not-main');
     const openMenuButton = document.querySelector('.menu-button.main');
 
-    openMenuButton.addEventListener('click', () => {
+    openMenuButton.addEventListener('click', (e) => {
         openMenuButton.classList.toggle('active');
         mobileMenuDIV.classList.toggle('active');
         menuButtons.forEach((button) => button.classList.toggle('active'));
     });
+
+    menuButtons.forEach((button) => button.addEventListener('click', (e) => {
+        menuButtons.forEach((button) => button.classList.remove('selected'));
+        e.currentTarget.classList.add('selected');
+
+        // redirect
+        location.href = e.currentTarget.dataset.redirect;
+    }))
 }
 
 initMenu();
